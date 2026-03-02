ae1c2e54b22654579f277e9da8e5de3a6d96011d
dveamer <dveamer@gmail.com>
2026-03-02 19:06:10 +0900

    camera, motion 충돌 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.dveamer.babysitter"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2026030100
-        versionName = "2026030100"
+        versionCode = 2026030200
+        versionName = "2026030200"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/dveamer/babysitter/monitor/MicrophoneMonitor.kt
+++ app/src/main/java/com/dveamer/babysitter/monitor/MicrophoneMonitor.kt
@@ -75,7 +75,6 @@ class MicrophoneMonitor(
                     .map { abs(it.toInt()) }
                     .average()
 
-                Log.d(TAG, "avgAmplitude : $avgAmplitude")
                 val active = avgAmplitude > amplitudeThreshold
                 mutableSignals.tryEmit(
                     MonitorSignal(
