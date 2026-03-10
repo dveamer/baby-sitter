735adc637eeab4916deccf1db1633e15204be577
dveamer <dveamer@gmail.com>
2026-03-11 00:26:09 +0900

    릴리즈 2026031101

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.dveamer.babysitter"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2026031100
-        versionName = "2026031100"
+        versionCode = 2026031101
+        versionName = "2026031101"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
