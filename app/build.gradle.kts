a3e1ade3ddc211fbeff7ce4de792e76a10cf98fc
dveamer <dveamer@gmail.com>
2026-05-07 01:04:53 +0900

    Remove deprecated edge-to-edge APIs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.google.zxing:core:3.5.3")
